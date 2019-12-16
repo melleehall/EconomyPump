63c8fa1e9ec1c84966cea9e09676c2209a3413b9
Melanie Hallowell <melleehall@gmail.com>
2019-12-16 16:06:17 -0700

    added jQuery event listeners to change views

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+
+
+
+// Event handlers
+
+function goButton () {
+    $('.go').click(function () {
+        $('.hero-img').addClass('hide');
+        $('.search-view').removeClass('hide');
+        console.log(`goButton function ran`)
+    });
+}
+
+function searchButton () {
+    $('.search').click(function () {
+        $('.search-view').addClass('hide');
+        $('.stats-view').removeClass('hide');
+        console.log(`goButton function ran`)
+    });
+}
+
+function shopButton () {
+    $('.shop').click(function () {
+        $('.stats-view').addClass('hide');
+        $('.shop-view').removeClass('hide');
+        console.log(`goButton function ran`)
+    });
+}
+
+function watchButtons () {
+    goButton();
+    searchButton();
+    shopButton();
+}
+
+$(watchButtons)
